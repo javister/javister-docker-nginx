eb527f80ada3d84f457342db743801ceb0c3a0ec
vektory79 <vektory79@gmail.com>
2017-12-30 00:59:07 +0300

    Параметризировано использование DHPARAM, т.к. генерация ключа - очень длительная операция и может сильно мешать на отладку

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM javister-docker-docker.bintray.io/javister/javister-docker-nginx-base:2
 MAINTAINER Viktor Verbitsky <vektory79@gmail.com>
 
 ENV RPMLIST="$RPMLIST certbot" \
-    LETSENCRYPT_EMAIL=""
+    LETSENCRYPT_EMAIL="" \
+    USE_DHPARAM="no"
 
 COPY files /
 
--- files/etc/my_init.d/35_setup_nginx.sh
+++ files/etc/my_init.d/35_setup_nginx.sh
@@ -14,7 +14,7 @@ if [ ! -d "/config/nginx" ]; then
     chown --recursive system:system /config/nginx
 fi
 
-if [ ! -d "/config/nginx/certs/dhparam.pem" ]; then
+if [ "${USE_DHPARAM}" == "yes" ] && [ ! -d "/config/nginx/certs/dhparam.pem" ]; then
     openssl dhparam -out /config/nginx/certs/dhparam.pem 4096
 fi
 
