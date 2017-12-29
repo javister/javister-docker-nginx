0a406cafb0e322291b65bc24001bed68ef35e896
vektory79 <vektory79@gmail.com>
2017-12-30 00:01:11 +0300

    Поддержка Let's Encrypt

---
+++ files/etc/my_init.d/36_setup_letsencrypt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+HOSTS="/config/letsencrypt/hosts.txt"
+
+[ ! -d "/config/letsencrypt" ] && mkdir --parents /config/letsencrypt || true
+
+if [ ! -f "${HOSTS}" ]; then
+    touch ${HOSTS}
+fi
+
+if [ "${LETSENCRYPT_EMAIL}" ]; then
+    sed --in-place "s/# email = .*/email = ${LETSENCRYPT_EMAIL}/g" /etc/letsencrypt/cli.ini
+fi
